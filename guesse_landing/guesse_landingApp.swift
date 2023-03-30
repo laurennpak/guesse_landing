5ebd6b2f1370a25a13c3fdea63cbea87ce3c56b0
Lauren <lauren09pak@gmail.com>
2023-03-30 19:06:27 -0400

    Initial Commit

---
+++ guesse_landing/guesse_landingApp.swift
@@ -0,0 +1,17 @@
+//
+//  guesse_landingApp.swift
+//  guesse_landing
+//
+//  Created by Lauren Pak on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct guesse_landingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
